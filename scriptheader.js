a46b651fb75693802e67835518525c05671c9e57
flavia sigoli <flaviasivm@gmail.com>
2025-01-09 12:24:57 -0300

    test2

--- scriptheader.js
+++ scriptheader.js
@@ -1,20 +1,18 @@
-// pin box-c when it reaches the center of the viewport, for 300px
-ScrollTrigger.create({
-    trigger: ".box-c",
-    pin: true,
-    start: "center center",
-    end: "+=300",
-    markers: true
+$(function(){
+  var lastScrollTop = 0, delta = 15;
+  $(window).scroll(function(event){
+     var st = $(this).scrollTop();
+     
+     if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+if ((st > lastScrollTop) && (lastScrollTop>0)) {
+     // downscroll code
+    $("header").css("top","-80px");
+
+ } else {
+    // upscroll code
+    $("header").css("top","0px");
+ }
+     lastScrollTop = st;
   });
-  
-  document.querySelector("button").addEventListener("click", (e) => {
-    // scroll to the spot where .box-c is in the center.
-    // parameters: element, smooth, position
-    smoother.scrollTo(".box-c", true, "center center");
-  
-    // or you could animate the scrollTop:
-    // gsap.to(smoother, {
-    //  scrollTop: smoother.offset(".box-c", "center center"),
-    //  duration: 1
-    // });
-  });
+});
